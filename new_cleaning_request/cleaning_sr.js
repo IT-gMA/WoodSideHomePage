32661a41d23b96f7bef086159972dff32269e8dc
IT-gMA <49443166+IT-gMA@users.noreply.github.com>
2023-04-10 22:08:51 +0800

    check for latest hour down to the last minute and seconds

--- new_cleaning_request/cleaning_sr.js
+++ new_cleaning_request/cleaning_sr.js
@@ -88,7 +88,7 @@ function _render_datetime_input_field(){
             const entered_datetime = convert_to_datetime($(this).val());
             //console.log(entered_datetime.getHours());
             //console.log(entered_datetime > max_time);
-            valid_datetime = !(min_time > entered_datetime || entered_datetime > max_time || entered_datetime.getHours() < EARLIEST || entered_datetime.getHours() + entered_datetime.getMinutes() + entered_datetime.getSeconds() > LATEST);
+            valid_datetime = !(min_time > entered_datetime || entered_datetime > max_time || entered_datetime.getHours() < EARLIEST || entered_datetime.getHours() + entered_datetime.getMinutes()/60 + entered_datetime.getSeconds()/3600 > LATEST);
             $(this).closest('.input-field-container').find('.input-err-msg').css('opacity', `${valid_datetime ? '0' : '1'}`);
             console.log(`valid_datetime: ${!(min_time > entered_datetime || entered_datetime > max_time)}`);
             console.log(`valid_hours: ${!(entered_datetime.getHours() < EARLIEST || entered_datetime.getHours() > LATEST)}`);
